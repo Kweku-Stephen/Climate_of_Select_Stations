da6086f91bae0ffaf786c1f433606367d1b55ac4
Stephen Aboagye-Ntow <aboagyeknerster@gmail.ocm>
2022-09-19 14:08:39 +0000

    Improved data import for tmax

--- Tmax Analysis for Nav_Koforidua.R
+++ Tmax Analysis for Nav_Koforidua.R
@@ -36,29 +36,54 @@ sapply(setdiff(pkgs, (.packages())), require, character.only = TRUE) # loading u
 dir(path = "~/Climate_of_Select_Stations/Max_Tmp", pattern = ".csv$", full.names = TRUE) |> # Reading in all datasets with names "Koforidu.csv, Kintampo.csv, navrong.csv"
   lapply(
     read.csv, # Reading in output of "dir"
-    na.strings = -99.9 # setting missing values coded as -99.9 to NA
+    na.strings = c("-99.9", "-99", "9988", "-9988", "9999", "-9999"), # setting missing values coded as -99.9 to NA
+    colClasses = c("tmax" = "numeric")
   ) |> # Piping the above output into the loop below
   lapply(
     # Looping the ananymous function below on each element of the list returned from above
     # to create a date object using columns "Year", "month" and "day" of each dataset
     \(data = "") {
-      # Creating a dataframe 
       data.frame(
-        Date = as.Date(paste(data[ ,1], data[ ,2], data[ ,3], sep = "-"), format = "%Y-%m-%d"),
-        Tmax = data[ ,"tmax"]
-      ) |> . => # Piping the output from above into the expression below 
-        .[.[ ,"Date"] >= "1980-01-01", ] # subseeting years >= 1980
+        Date = as.Date(
+          paste(
+            data[ ,grep("[Yy][Ee][Aa][Rr]", names(data), value = TRUE)],
+            data[ ,grep("[Mm][Oo][Nn][Tt]", names(data), value = TRUE)],
+            data[ ,grep("[Dd][Aa][Yy]", names(data), value = TRUE)],
+            sep = "-"
+          ),
+          format = "%Y-%m-%e"
+        ),
+      Tmax = data[ ,"tmax"]
+      ) |> . =>
+        .[.[ ,"Date"] >= "1980-01-01", ]
     }
   ) |> 
   # Renaming the list/output above (column names of each data in the list piped form the expression above)
-  setNames(c("Koforidua", "Navrongo")) -> Stations_Tmax # 
+  setNames(dir(path = "~/Climate_of_Select_Stations/Max_Tmp", pattern = ".csv$")) -> Stations_Tmax # 
+
+
+# # Creating a dataframe 
+# data.frame(
+#   Date = as.Date(
+#     paste(
+#       data[ ,grep("[Yy][Ee][Aa][Rr]", names(data), value = TRUE)], 
+#       data[ ,grep("[Mm][Oo][Nn][Tt][Hh]", names(data), value = TRUE)], 
+#       data[ ,grep("[Dd][Aa][Yy][Rr]", names(data), value = TRUE)], 
+#       sep = "-"
+#     ),
+#     format = "%Y-%m-%e"
+#   ),
+#   Tmax = data[ ,"tmax"]
+# ) |> . => # Piping the output from above into the expression below 
+#   .[.[ ,"Date"] >= "1980-01-01", ] # subseeting years >= 1980
+
 
 # for efficiency, use rio::import_list instead of combining "lapply" and "read.csv" to read the datasets into R
 
-# Changing class of Koforidua Rain variable to "numeric"
-#Stations[["Koforidua"]][ ,"Rain"] <- as.numeric(Stations[["Koforidua"]][ ,"Rain"])
-
-
+# # Converting Cape coast Tmax observations to numeric class
+# Stations_Tmax[["capecoast_tmax.csv"]][ ,"Tmax"] <- as.numeric(
+#   Stations_Tmax[["capecoast_tmax.csv"]][ ,"Tmax"]
+# )
 
 
 # Percentage of Missing Values for each year conditioned on Met Stations ####
@@ -73,10 +98,10 @@ lapply(
     `NA_%_1`
   ) #-> perc_NA_tmax
 
+require(magrittr)
 
 
-
-# Computing Annual Minimum Temperature ####
+# Computing Annual Maximum Temperature ####
 lapply(
   Stations_Tmax,
   # Ananymous function to compute mean annual max temp as a function of year
@@ -94,38 +119,55 @@ lapply(
   }
 ) -> Annual_Max_Temp # Name of the returned list form the looping construct above
 
-# Setting annual mean temp values for the year 2007 and 2008 from Koforidua because missing data is above 50%
-Annual_Max_Temp[["Koforidua"]][27:28, 2] <- NA
 
-# Imputing missing values the kalman filter 
-Annual_Max_Temp[["Koforidua"]][ ,"Tmax"] <- imputeTS::na_kalman(
-  Annual_Max_Temp[["Koforidua"]][ ,"Tmax"], 
+#Insering missing year of 2008
+dplyr::full_join(Annual_Max_Temp$koforid_tmax.csv, data.frame(
+  Year = 1980:2021
+)) %>%
+  .[order(.[, "Year"]), ] -> Annual_Max_Temp$koforid_tmax.csv
+
+# Setting annual mean temp values for the year 2007 and 2008 from Koforidua because missing data is above 50%
+Annual_Max_Temp[["koforid_tmax.csv"]][27:28, 2] <- NA
+# Cape Coast
+Annual_Max_Temp[["capecoast_tmax.csv"]][c(33, 34, 36), "Tmax"] <- NA
+
+# Imputing missing values the kalman filter for Koforidua
+Annual_Max_Temp[["koforid_tmax.csv"]][ ,"Tmax"] <- imputeTS::na_kalman(
+  Annual_Max_Temp[["koforid_tmax.csv"]][ ,"Tmax"], 
+  model = "StructTS", 
+  smooth = TRUE,
+  nit = -1
+)
+
+# Imputing missing values for Cape Coast using kalman filter
+Annual_Max_Temp[["capecoast_tmax.csv"]][ ,"Tmax"] <- imputeTS::na_kalman(
+  Annual_Max_Temp[["capecoast_tmax.csv"]][ ,"Tmax"], 
   model = "StructTS", 
   smooth = TRUE,
   nit = -1
 )
 
 
-# Plotting Annual Min Temp
-ggplot(data = Annual_Max_Temp[["Koforidua"]], aes(x = Year, y = Tmax)) + 
-  geom_line(lwd = 2, col = "firebrick") +
-  #scale_x_date(date_labels = "%Y", date_breaks = "4 years") +
-  ggtitle("Mean Maximum Temperature of Koforidua \n 1980 - 2021") +
-  xlab("Month") +
-  ylab(expression("Temp ("~degree*C*")")) +
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20)) -> Koforidua_anntmax
-
-ggplot(data = Annual_Max_Temp[["Navrongo"]], aes(x = Year, y = Tmax)) +
-  geom_line(lwd = 2, col = "firebrick") +
-  #scale_x_date(date_labels = "%Y", date_breaks = "4 years") +
-  ggtitle("Mean Maximum Temperature of Navrongo \n 1980 - 2021") +
-  xlab("Month") +
-  ylab(expression("Temp ("~degree*C*")")) +
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20)) -> Navrongo_anntmax
+# # Plotting Annual Min Temp
+# ggplot(data = Annual_Max_Temp[["Koforidua"]], aes(x = Year, y = Tmax)) + 
+#   geom_line(lwd = 2, col = "firebrick") +
+#   #scale_x_date(date_labels = "%Y", date_breaks = "4 years") +
+#   ggtitle("Mean Maximum Temperature of Koforidua \n 1980 - 2021") +
+#   xlab("Month") +
+#   ylab(expression("Temp ("~degree*C*")")) +
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20)) -> Koforidua_anntmax
+# 
+# ggplot(data = Annual_Max_Temp[["Navrongo"]], aes(x = Year, y = Tmax)) +
+#   geom_line(lwd = 2, col = "firebrick") +
+#   #scale_x_date(date_labels = "%Y", date_breaks = "4 years") +
+#   ggtitle("Mean Maximum Temperature of Navrongo \n 1980 - 2021") +
+#   xlab("Month") +
+#   ylab(expression("Temp ("~degree*C*")")) +
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20)) -> Navrongo_anntmax
 
 
 # Summary Stats for Annual Max Temp 
@@ -174,25 +216,25 @@ lapply(
 
 
 # Plotting mean monthly Tmax
-ggplot(data = Monthly_Tmax[["Koforidua"]], aes(x = Date, y = Tmax)) + 
-  geom_line(lwd = 2, col = "firebrick") +
-  scale_x_date(date_labels = "%b", date_breaks = "1 month") +
-  ggtitle("Monthly Maximum Temperature of Koforidua \n 1980 - 2021") +
-  xlab("Month") +
-  ylab(expression("Temp ("~degree*C*")")) +
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20)) -> Koforidua_mnthlytmax
-
-ggplot(data = Monthly_Tmax[["Navrongo"]], aes(x = Date, y = Tmax)) +
-  geom_line(lwd = 2, col = "firebrick") +
-  scale_x_date(date_labels = "%b", date_breaks = "1 month") +
-  ggtitle("Monthly Maximum Temperature of Navrongo \n 1980 - 2021") +
-  xlab("Month") +
-  ylab(expression("Temp ("~degree*C*")")) +
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20)) -> Navrongo_mnthlytmax
+# ggplot(data = Monthly_Tmax[["Koforidua"]], aes(x = Date, y = Tmax)) + 
+#   geom_line(lwd = 2, col = "firebrick") +
+#   scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+#   ggtitle("Monthly Maximum Temperature of Koforidua \n 1980 - 2021") +
+#   xlab("Month") +
+#   ylab(expression("Temp ("~degree*C*")")) +
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20)) -> Koforidua_mnthlytmax
+# 
+# ggplot(data = Monthly_Tmax[["Navrongo"]], aes(x = Date, y = Tmax)) +
+#   geom_line(lwd = 2, col = "firebrick") +
+#   scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+#   ggtitle("Monthly Maximum Temperature of Navrongo \n 1980 - 2021") +
+#   xlab("Month") +
+#   ylab(expression("Temp ("~degree*C*")")) +
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20)) -> Navrongo_mnthlytmax
 
 # Summary Stats for mean monthly max temp
 sink("Data_outputs/summary statisitcs.txt", append = TRUE, split = TRUE)
@@ -238,36 +280,50 @@ Annual_MaxTmp_Anomaly <- lapply(
     }
   )
 
+# Inseting missing year of 2008 for koforidua
+Annual_MaxTmp_Anomaly[["koforid_tmax.csv"]] <- 
+  dplyr::full_join(
+    Annual_MaxTmp_Anomaly[["koforid_tmax.csv"]],
+    data.frame(Year = 1980:2021)
+) |> . =>
+  .[order(.[ ,"Year"]), ] |> . =>
+  transform(
+    .,
+    Deviations = imputeTS::na_kalman(
+      .[, "Deviations"],
+      smooth = TRUE, 
+      nit = -1
+    )
+  )
 
-
-# Koforidua Rainfall Anomaly
-ggplot(data = Annual_MaxTmp_Anomaly[["Koforidua"]], aes(x = Year, y = Deviations, ymin = 0, ymax = Deviations)) +
-  geom_linerange(data = Annual_MaxTmp_Anomaly[["koforidua"]], aes(
-    colour = ifelse(Deviations > 0, "Positive", "Negative")),
-    stat = "identity", position = "identity", size = 4) +
-  geom_hline(yintercept = 0) +
-  labs(title = "Max Tmp Anomaly of Koforidua \n 1980 - 2021", x = "Year", y = expression("Temp ("~degree*C*")")) +
-  scale_x_continuous(breaks = seq(from = 1980, to = 2021, by = 7)) +
-  theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 30),
-        axis.title = element_text(size = 28, face = "bold"),
-        axis.text = element_text(size = 26),
-        legend.title = element_blank(),
-        legend.text = element_text(size = 23))
-
-
-# Navrongo Rainfall Anomaly
-ggplot(data = Annual_MaxTmp_Anomaly[["Navrongo"]], aes(x = Year, y = Deviations, ymin = 0, ymax = Deviations)) +
-  geom_linerange(data = Annual_MaxTmp_Anomaly[["Navrongo"]], aes(
-    colour = ifelse(Deviations > 0, "Positive", "Negative")),
-    stat = "identity", position = "identity", size = 4) +
-  geom_hline(yintercept = 0) +
-  labs(title = "Max Tmp Anomaly of Navrongo \n 1980 - 2021", x = "Year", y = expression("Temp ("~degree*C*")")) +
-  scale_x_continuous(breaks = seq(from = 1944, to = 2021, by = 11)) +
-  theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 30),
-        axis.title = element_text(size = 28, face = "bold"),
-        axis.text = element_text(size = 26),
-        legend.title = element_blank(),
-        legend.text = element_text(size = 23))
+# # Koforidua Rainfall Anomaly
+# ggplot(data = Annual_MaxTmp_Anomaly[["Koforidua"]], aes(x = Year, y = Deviations, ymin = 0, ymax = Deviations)) +
+#   geom_linerange(data = Annual_MaxTmp_Anomaly[["koforidua"]], aes(
+#     colour = ifelse(Deviations > 0, "Positive", "Negative")),
+#     stat = "identity", position = "identity", size = 4) +
+#   geom_hline(yintercept = 0) +
+#   labs(title = "Max Tmp Anomaly of Koforidua \n 1980 - 2021", x = "Year", y = expression("Temp ("~degree*C*")")) +
+#   scale_x_continuous(breaks = seq(from = 1980, to = 2021, by = 7)) +
+#   theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 30),
+#         axis.title = element_text(size = 28, face = "bold"),
+#         axis.text = element_text(size = 26),
+#         legend.title = element_blank(),
+#         legend.text = element_text(size = 23))
+# 
+# 
+# # Navrongo Rainfall Anomaly
+# ggplot(data = Annual_MaxTmp_Anomaly[["Navrongo"]], aes(x = Year, y = Deviations, ymin = 0, ymax = Deviations)) +
+#   geom_linerange(data = Annual_MaxTmp_Anomaly[["Navrongo"]], aes(
+#     colour = ifelse(Deviations > 0, "Positive", "Negative")),
+#     stat = "identity", position = "identity", size = 4) +
+#   geom_hline(yintercept = 0) +
+#   labs(title = "Max Tmp Anomaly of Navrongo \n 1980 - 2021", x = "Year", y = expression("Temp ("~degree*C*")")) +
+#   scale_x_continuous(breaks = seq(from = 1944, to = 2021, by = 11)) +
+#   theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 30),
+#         axis.title = element_text(size = 28, face = "bold"),
+#         axis.text = element_text(size = 26),
+#         legend.title = element_blank(),
+#         legend.text = element_text(size = 23))
 
 # Composites of Raifnall Anomaly
 gridExtra::grid.arrange(
@@ -317,29 +373,45 @@ lapply(
 ) -> max_maximum_temp
 
 # Replacing the year 2007's Count with NA
-max_maximum_temp[["Koforidua"]][max_maximum_temp[["Koforidua"]][ ,"Year"] == 2007, "Tmax"] <- NA
+max_maximum_temp[["koforid_tmax.csv"]][
+  max_maximum_temp[["koforid_tmax.csv"]][ ,"Year"] == 2007, "Tmax"
+] <- NA
+
+# Replacing the years 2021, 2013 an 2015 with NAs
+max_maximum_temp[["capecoast_tmax.csv"]][
+  max_maximum_temp[["capecoast_tmax.csv"]][ ,"Year"] %in% c(2012, 2013, 2015), "Tmax"
+] <- NA
 
 # Imputing missing values in the Koforidua max tmp dataframe with the max_maximum_temp list
-max_maximum_temp[["Koforidua"]][ ,"Tmax"] <- imputeTS::na_kalman(
-    max_maximum_temp[["Koforidua"]][ ,"Tmax"],
+max_maximum_temp[["koforid_tmax.csv"]][ ,"Tmax"] <- imputeTS::na_kalman(
+    max_maximum_temp[["koforid_tmax.csv"]][ ,"Tmax"],
     model = "StructTS", 
     smooth = TRUE,
     nit = -1
   )
 
+# Imputing missing values in the capecoast max tmp dataframe with the max_maximum_temp list
+max_maximum_temp[["capecoast_tmax.csv"]][ ,"Tmax"] <- imputeTS::na_kalman(
+  max_maximum_temp[["capecoast_tmax.csv"]][ ,"Tmax"],
+  model = "StructTS", 
+  smooth = TRUE,
+  nit = -1
+)
 
-# Plotting 
-ggplot(data = max_maximum_temp[["Koforidua"]], aes(x = Year, y = Tmax)) +
-  geom_line(col = "firebrick", lwd = 2) +
-  scale_x_continuous(breaks = seq(1980, 2021, by = 5)) +
-  labs(
-    title = "Number of Days with Max Tmp in the 90th percentile \n 1980 - 2021", 
-    x = "Year", 
-    y = "Number of Days"
-  ) + 
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20))
+
+
+# # Plotting 
+# ggplot(data = max_maximum_temp[["Koforidua"]], aes(x = Year, y = Tmax)) +
+#   geom_line(col = "firebrick", lwd = 2) +
+#   scale_x_continuous(breaks = seq(1980, 2021, by = 5)) +
+#   labs(
+#     title = "Number of Days with Max Tmp in the 90th percentile \n 1980 - 2021", 
+#     x = "Year", 
+#     y = "Number of Days"
+#   ) + 
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20))
 
 # Summary Statistics of Maximum Temperature ####
 sink("Data_outputs/summary statisitcs.txt", append = TRUE, split = TRUE)
@@ -377,28 +449,44 @@ lapply(
 ) -> min_maximum_temp
 
 # Replacing the year 2007's Count with NA
-min_maximum_temp[["Koforidua"]][min_maximum_temp[["Koforidua"]][ ,"Year"] == 2007, "Tmax"] <- NA
+min_maximum_temp[["koforid_tmax.csv"]][
+  min_maximum_temp[["koforid_tmax.csv"]][ ,"Year"] == 2007, "Tmax"
+] <- NA
+
+# Replacing the years 2012, 2013, and 2015 Tmaxs with NA
+min_maximum_temp[["capecoast_tmax.csv"]][
+  min_maximum_temp[["capecoast_tmax.csv"]][ ,"Year"] %in% c(2012, 2013, 2015), "Tmax"
+] <- NA
 
 # Imputing missing values in the Koforidua max tmp dataframe with the max_maximum_temp list
-min_maximum_temp[["Koforidua"]][ ,"Tmax"] <- imputeTS::na_kalman(
-  min_maximum_temp[["Koforidua"]][ ,"Tmax"],
+min_maximum_temp[["koforid_tmax.csv"]][ ,"Tmax"] <- imputeTS::na_kalman(
+  min_maximum_temp[["koforid_tmax.csv"]][ ,"Tmax"],
   model = "StructTS", 
   smooth = TRUE,
   nit = -1
 )
 
+# Imputing missing values in the cape coast max tmp dataframe with the max_maximum_temp list
+min_maximum_temp[["capecoast_tmax.csv"]][ ,"Tmax"] <- imputeTS::na_kalman(
+  min_maximum_temp[["capecoast_tmax.csv"]][ ,"Tmax"],
+  model = "StructTS", 
+  smooth = TRUE,
+  nit = -1
+)
+
+
 # Plotting 
-ggplot(data = min_maximum_temp[["Koforidua"]], aes(x = Year, y = Tmax)) +
-  geom_line(col = "firebrick", lwd = 2) +
-  scale_x_continuous(breaks = seq(1980, 2021, by = 5)) +
-  labs(
-    title = "Number of Days with Max Tmp in the 10th percentile \n 1980 - 2021", 
-    x = "Year", 
-    y = "Number of Days"
-  ) + 
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20))
+# ggplot(data = min_maximum_temp[["Koforidua"]], aes(x = Year, y = Tmax)) +
+#   geom_line(col = "firebrick", lwd = 2) +
+#   scale_x_continuous(breaks = seq(1980, 2021, by = 5)) +
+#   labs(
+#     title = "Number of Days with Max Tmp in the 10th percentile \n 1980 - 2021", 
+#     x = "Year", 
+#     y = "Number of Days"
+#   ) + 
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20))
 
 
 # Summary Statistics of Maximum Temperature ####
