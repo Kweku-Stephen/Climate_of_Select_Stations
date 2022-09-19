2045b2af4778d8bd9f25c495ecbdf8dae2b3507b
Stephen Aboagye-Ntow <aboagyeknerster@gmail.ocm>
2022-09-19 14:09:04 +0000

    Improved Data import for Tmin

--- Tmin Analysis for Nav_Koforidua.R
+++ Tmin Analysis for Nav_Koforidua.R
@@ -1,3 +1,6 @@
+############################################################################################################
+# Sourcing functions Script ####
+Source("Functions Script.R")
 # Creating Directories ####
 for ( i in c("Data_outputs", "Plots_outputs")){
   if (!dir.exists(i)) dir.create(i) else message("Already Created")
@@ -31,25 +34,39 @@ sapply(setdiff(pkgs, (.packages())), require, character.only = TRUE)
 dir(path = "~/Climate_of_Select_Stations/Min_Tmp", pattern = ".csv$", full.names = TRUE) |> # Reading in all datasets with names "Koforidu.csv, Kintampo.csv, navrong.csv"
   lapply(
     read.csv, # Reading in output of "dir"
-    na.strings = -99.9
+    na.strings = c("-99.9", "99.9", "9988", "-9988", "-9999", "9999"),
+    colClasses = c("tmin" = "numeric")
   ) |> 
   lapply(
     # Looping the function below on each element of the list returned from above
     # to create a date object with columns 1, 2 and 3 of each dataset
     \(data = "") {
       data.frame(
-        Date = as.Date(paste(data[ ,1], data[ ,2], data[ ,3], sep = "-"), format = "%Y-%m-%d"),
+        Date = as.Date(
+          paste(
+            data[ ,grep("[Yy][Ee][Aa][Rr]", names(data), value = TRUE)],
+            data[ ,grep("[Mm][Oo][Nn][Tt]", names(data), value = TRUE)],
+            data[ ,grep("[Dd][Aa][Yy]", names(data), value = TRUE)],
+            sep = "-"
+          ),
+          format = "%Y-%m-%e"
+        ),
         Tmin = data[ ,"tmin"]
       ) |> . =>
         .[.[ ,"Date"] >= "1980-01-01", ]
     }
   ) |> 
-  setNames(c("Koforidua", "Navrongo")) -> Stations_Tmin # Renaming the list/output above
+  setNames(dir(path = "~/climate_of_Select_Stations/Min_Tmp", pattern = ".csv$")) -> Stations_Tmin # Renaming the list/output above
 
 # for efficiency, use rio::import_list instead of combining "lapply" and "read.csv" to read the datasets into R
 
 # Changing class of Koforidua Rain variable to "numeric"
 #Stations[["Koforidua"]][ ,"Rain"] <- as.numeric(Stations[["Koforidua"]][ ,"Rain"])
+
+# # Converting cape coast tmin to class numeric
+# Stations_Tmin[["capecoast_tmin.csv"]][ ,"Tmin"] <- as.numeric(
+#   Stations_Tmin[["capecoast_tmin.csv"]][ ,"Tmin"]
+# )
 
 
 # Percentage of Missing Values for each year conditioned on Met Stations ####
@@ -81,28 +98,43 @@ lapply(
   }
 ) -> Annual_Min_Temp
 
-# Plotting Annual Min Temp
+# Replacing the years 2012, 2013 and 2015 tmaxs with NA
+Annual_Min_Temp[["capecoast_tmin.csv"]][
+  Annual_Min_Temp[["capecoast_tmin.csv"]][ ,"Year"] %in% c(2012, 2013, 2015), "Tmin"
+] <- NA
 
-# Plotting Annual Min Temp
-ggplot(data = Annual_Min_Tmp[["Koforidua"]], aes(x = Year, y = Tmin)) + 
-  geom_line(lwd = 2, col = "firebrick") +
-  scale_x_date(date_labels = "%Y", date_breaks = "4 years") +
-  ggtitle("Minimum Temperature of Koforidua \n 1980 - 2021") +
-  xlab("Month") +
-  ylab(expression("Temp ("~degree*C*")")) +
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20)) -> Koforidua_anntmin
+# Imputing missing values for cape coast annual min tmp using kalman filter
+Annual_Min_Temp[["capecoast_tmin.csv"]][ ,"Tmin"] %<>% 
+  imputeTS::na_kalman(
+    model = "StructTS",
+    smooth = TRUE,
+    nit = -1
+  )
 
-ggplot(data = Annual_min_Tmp[["Navrongo"]], aes(x = Year, y = Tmin)) +
-  geom_line(lwd = 2, col = "firebrick") +
-  scale_x_date(date_labels = "%Y", date_breaks = "4 years") +
-  ggtitle("Monthly Minimum Temperature of Navrongo \n 1980 - 2021") +
-  xlab("Month") +
-  ylab(expression("Temp ("~degree*C*")")) +
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20)) -> Navrongo_anntmin
+
+
+# # Plotting Annual Min Temp
+# 
+# # Plotting Annual Min Temp
+# ggplot(data = Annual_Min_Tmp[["Koforidua"]], aes(x = Year, y = Tmin)) + 
+#   geom_line(lwd = 2, col = "firebrick") +
+#   scale_x_date(date_labels = "%Y", date_breaks = "4 years") +
+#   ggtitle("Minimum Temperature of Koforidua \n 1980 - 2021") +
+#   xlab("Month") +
+#   ylab(expression("Temp ("~degree*C*")")) +
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20)) -> Koforidua_anntmin
+# 
+# ggplot(data = Annual_min_Tmp[["Navrongo"]], aes(x = Year, y = Tmin)) +
+#   geom_line(lwd = 2, col = "firebrick") +
+#   scale_x_date(date_labels = "%Y", date_breaks = "4 years") +
+#   ggtitle("Monthly Minimum Temperature of Navrongo \n 1980 - 2021") +
+#   xlab("Month") +
+#   ylab(expression("Temp ("~degree*C*")")) +
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20)) -> Navrongo_anntmin
 
 # Composites of Mean Monthly Rainfall
 # Kintampo / Koforidua / Navrongo
@@ -158,29 +190,29 @@ lapply(
   ) -> Monthly_Tmin
 
 
-# Date for x-axis
-Date <- seq(as.Date("1990-01-01"), as.Date("1990-12-31"), by = "month")
-
-# Koforidua mean monthly min tmp
-ggplot(data = Monthly_Tmin[["Koforidua"]], aes(x = Date, y = Tmin)) + 
-  geom_line(lwd = 2, col = "firebrick") +
-  scale_x_date(date_labels = "%b", date_breaks = "1 month") +
-  ggtitle("Monthly Min Tmp of Koforidua \n 1980 - 2021") +
-  xlab("Month") +
-  ylab(expression("Temp ("~degree*C*")")) +
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20)) 
-
-ggplot(data = Monthly_Tmin[["Navrongo"]], aes(x = Date, y = Tmin)) +
-  geom_line(lwd = 2, col = "darkblue") +
-  scale_x_date(date_labels = "%b", date_breaks = "1 month") +
-  ggtitle("Mean Monthly Rainfall of Navrongo \n 1946 - 2021") +
-  xlab("Month") +
-  ylab("Rainfall (mm)") +
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20))
+# # Date for x-axis
+# Date <- seq(as.Date("1990-01-01"), as.Date("1990-12-31"), by = "month")
+# 
+# # Koforidua mean monthly min tmp
+# ggplot(data = Monthly_Tmin[["Koforidua"]], aes(x = Date, y = Tmin)) + 
+#   geom_line(lwd = 2, col = "firebrick") +
+#   scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+#   ggtitle("Monthly Min Tmp of Koforidua \n 1980 - 2021") +
+#   xlab("Month") +
+#   ylab(expression("Temp ("~degree*C*")")) +
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20)) 
+# 
+# ggplot(data = Monthly_Tmin[["Navrongo"]], aes(x = Date, y = Tmin)) +
+#   geom_line(lwd = 2, col = "darkblue") +
+#   scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+#   ggtitle("Mean Monthly Rainfall of Navrongo \n 1946 - 2021") +
+#   xlab("Month") +
+#   ylab("Rainfall (mm)") +
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20))
 
 
 # Summary Statistics for Mean monthly min temp
@@ -227,19 +259,20 @@ Annual_MinTmp_Anomaly <- lapply(
     }
   )
 
+# 
 
-ggplot(data = Annual_MinTmp_Anomaly[["Koforidua"]], aes(x = Year, y = Deviations, ymin = 0, ymax = Deviations)) +
-  geom_linerange(data = Annual_MinTmp_Anomaly[["koforidua"]], aes(
-    colour = ifelse(Deviations > 0, "Positive", "Negative")),
-    stat = "identity", position = "identity", size = 4) +
-  geom_hline(yintercept = 0) +
-  labs(title = "Min Tmp Anomaly of Koforidua \n 1980 - 2021", x = "Year", y = expression("Temp ("~degree*C*")")) +
-  #scale_x_continuous(breaks = seq(from = 1980, to = 2021, by = 7)) +
-  theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 30),
-        axis.title = element_text(size = 28, face = "bold"),
-        axis.text = element_text(size = 26),
-        legend.title = element_blank(),
-        legend.text = element_text(size = 23))
+# ggplot(data = Annual_MinTmp_Anomaly[["Koforidua"]], aes(x = Year, y = Deviations, ymin = 0, ymax = Deviations)) +
+#   geom_linerange(data = Annual_MinTmp_Anomaly[["koforidua"]], aes(
+#     colour = ifelse(Deviations > 0, "Positive", "Negative")),
+#     stat = "identity", position = "identity", size = 4) +
+#   geom_hline(yintercept = 0) +
+#   labs(title = "Min Tmp Anomaly of Koforidua \n 1980 - 2021", x = "Year", y = expression("Temp ("~degree*C*")")) +
+#   #scale_x_continuous(breaks = seq(from = 1980, to = 2021, by = 7)) +
+#   theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 30),
+#         axis.title = element_text(size = 28, face = "bold"),
+#         axis.text = element_text(size = 26),
+#         legend.title = element_blank(),
+#         legend.text = element_text(size = 23))
 
 # Summay Statistics for min tmp anomaly
 sink("Data_outputs/summary statisitcs.txt", append = TRUE, split = TRUE)
@@ -278,19 +311,32 @@ lapply(
   }
 ) -> max_minimum_temp
 
+# Setting the count values of the years 2012, 2013 and 2015 to NAs
+max_minimum_temp[["capecoast_tmin.csv"]][
+  max_minimum_temp[["capecoast_tmin.csv"]][ ,"Year"] %in% c(2012, 2013, 2015), "Tmin"
+] <- NA
+
+# Missing value imputation for cape coast's max of minimum temp 
+max_minimum_temp[["capecoast_tmin.csv"]][ ,"Tmin"] %<>%
+  imputeTS::na_kalman(
+    model = "StructTS",
+    smooth = TRUE,
+    nit = -1
+  )
+
 
 # Plotting 
-ggplot(data = max_minimum_temp[["Koforidua"]], aes(x = Year, y = Tmin)) +
-  geom_line(col = "firebrick", lwd = 2) +
-  #scale_x_continuous(breaks = seq(1980, 2021, by = 5)) +
-  labs(
-    title = "Number of Days with Min Tmp in the 90th percentile \n 1980 - 2021", 
-    x = "Year", 
-    y = expression("Temperature("~degree*C*")")
-  ) + 
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20))
+# ggplot(data = max_minimum_temp[["Koforidua"]], aes(x = Year, y = Tmin)) +
+#   geom_line(col = "firebrick", lwd = 2) +
+#   #scale_x_continuous(breaks = seq(1980, 2021, by = 5)) +
+#   labs(
+#     title = "Number of Days with Min Tmp in the 90th percentile \n 1980 - 2021", 
+#     x = "Year", 
+#     y = expression("Temperature("~degree*C*")")
+#   ) + 
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20))
 
 # summary Statistics of max of minimum temperature
 sink("Data_outputs/summary statisitcs.txt", append = TRUE, split = TRUE)
@@ -329,18 +375,32 @@ lapply(
   }
 ) -> min_minimum_temp
 
-# Plotting 
-ggplot(data = min_minimum_temp[["Koforidua"]], aes(x = Year, y = Tmin)) +
-  geom_line(col = "firebrick", lwd = 2) +
-  #scale_x_continuous(breaks = seq(1980, 2021, by = 5)) +
-  labs(
-    title = "Number of Days with Min Tmp in the 10th percentile \n 1980 - 2021", 
-    x = "Year", 
-    y = expression("Temperature("~degree*C*")")
-  ) + 
-  theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 22, face = "bold"),
-        axis.text = element_text(size = 20))
+# Setting the count values of the years 2012, 2013 and 2015 to NAs
+min_minimum_temp[["capecoast_tmin.csv"]][
+  min_minimum_temp[["capecoast_tmin.csv"]][ ,"Year"] %in% c(2012, 2013, 2015), "Tmin"
+] <- NA
+
+# Missing value imputation for cape coast's max of minimum temp 
+min_minimum_temp[["capecoast_tmin.csv"]][ ,"Tmin"] %<>%
+  imputeTS::na_kalman(
+    model = "StructTS",
+    smooth = TRUE,
+    nit = -1
+  )
+
+
+# # Plotting 
+# ggplot(data = min_minimum_temp[["Koforidua"]], aes(x = Year, y = Tmin)) +
+#   geom_line(col = "firebrick", lwd = 2) +
+#   #scale_x_continuous(breaks = seq(1980, 2021, by = 5)) +
+#   labs(
+#     title = "Number of Days with Min Tmp in the 10th percentile \n 1980 - 2021", 
+#     x = "Year", 
+#     y = expression("Temperature("~degree*C*")")
+#   ) + 
+#   theme(plot.title = element_text(size = 24, face = "bold", hjust = 0.5),
+#         axis.title = element_text(size = 22, face = "bold"),
+#         axis.text = element_text(size = 20))
 
 
 sink("Data_outputs/summary statisitcs.txt", append = TRUE, split = TRUE)
